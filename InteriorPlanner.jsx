34536ebd7820695323b654b88c1c60c95e327fbc
20231432-glitch <20231432@sungshin.ac.kr>
2026-05-15 00:31:48 +0900

    upload

--- InteriorPlanner.jsx
+++ InteriorPlanner.jsx
@@ -73,3 +73,37 @@ const InteriorPlanner = () => {
 };
 
 export default InteriorPlanner;
+
+
+// 1. 두 사각형의 충돌 여부를 확인하는 함수 
+const checkCollision = (rect1, rect2) => {
+  return (
+    rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y < rect2.y + rect2.height &&
+    rect1.y + rect1.height > rect2.y
+  );
+};
+
+// 2. 새로운 위치가 유효한지 검사하는 함수 [cite: 34, 35, 36]
+const isValidPosition = (movingItem, allItems, roomBoundary) => {
+  // A. 방 경계를 벗어나는지 확인 (벽 충돌 방지) [cite: 35, 96]
+  if (
+    movingItem.x < roomBoundary.x ||
+    movingItem.y < roomBoundary.y ||
+    movingItem.x + movingItem.width > roomBoundary.x + roomBoundary.width ||
+    movingItem.y + movingItem.height > roomBoundary.y + roomBoundary.height
+  ) {
+    return false;
+  }
+
+  // B. 다른 가구와 겹치는지 확인 [cite: 36, 105]
+  for (let item of allItems) {
+    if (item.id === movingItem.id) continue; // 자기 자신은 제외
+    if (checkCollision(movingItem, item)) {
+      return false;
+    }
+  }
+
+  return true;
+};
